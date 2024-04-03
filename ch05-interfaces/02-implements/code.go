f8f61e4450eadeeb4939a6e147033ed3a96fa1f5
janobono <janobono@gmail.com>
2024-04-03 08:22:59 +0200

    CH05 implements excercise
    Signed-off-by: janobono <janobono@gmail.com>

--- ch05-interfaces/02-implements/code.go
+++ ch05-interfaces/02-implements/code.go
@@ -19,6 +19,10 @@ func (c contractor) getName() string {
 	return c.name
 }
 
+func (c contractor) getSalary() int {
+	return c.hoursPerYear * c.hourlyPay
+}
+
 // ?
 
 // don't touch below this line
