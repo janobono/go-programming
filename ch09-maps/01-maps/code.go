5765c2cb599bb86f74822e7b426ce403be6981f0
janobono <janobono@gmail.com>
2024-04-03 16:55:25 +0200

    CH09 maps excercise
    Signed-off-by: janobono <janobono@gmail.com>

--- ch09-maps/01-maps/code.go
+++ ch09-maps/01-maps/code.go
@@ -1,11 +1,27 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 )
 
 func getUserMap(names []string, phoneNumbers []int) (map[string]user, error) {
 	// ?
+	if len(names) != len(phoneNumbers) {
+		return nil, errors.New("invalid sizes")
+	}
+
+	result := make(map[string]user)
+
+	for i := 0; i < len(names); i++ {
+		user := user{
+			name:        names[i],
+			phoneNumber: phoneNumbers[i],
+		}
+		result[user.name] = user
+	}
+
+	return result, nil
 }
 
 // don't touch below this line
