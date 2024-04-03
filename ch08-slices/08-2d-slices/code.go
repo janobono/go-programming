79c00355a212c974663fcd8546d0584c37bc22a9
janobono <janobono@gmail.com>
2024-04-03 13:39:38 +0200

    CH08 2d slices excercise
    Signed-off-by: janobono <janobono@gmail.com>

--- ch08-slices/08-2d-slices/code.go
+++ ch08-slices/08-2d-slices/code.go
@@ -4,6 +4,15 @@ import "fmt"
 
 func createMatrix(rows, cols int) [][]int {
 	// ?
+	matrix := make([][]int, 0)
+	for i := 0; i < rows; i++ {
+		row := make([]int, 0)
+		for j := 0; j < cols; j++ {
+			row = append(row, i*j)
+		}
+		matrix = append(matrix, row)
+	}
+	return matrix
 }
 
 // dont edit below this line
