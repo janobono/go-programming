2580b7362a9983d827ed6008fe684e946e871fa1
janobono <janobono@gmail.com>
2024-04-03 11:52:58 +0200

    CH07 loops fizzbuzz excercise
    Signed-off-by: janobono <janobono@gmail.com>

--- ch07-loops/04-loops-fizzbuzz/code.go
+++ ch07-loops/04-loops-fizzbuzz/code.go
@@ -1,7 +1,25 @@
 package main
 
+import (
+	"fmt"
+)
+
 func fizzbuzz() {
 	// ?
+	for i := 1; i <= 100; i++ {
+		output := ""
+		if i%3 == 0 {
+			output += "fizz"
+		}
+		if i%5 == 0 {
+			output += "buzz"
+		}
+		if output != "" {
+			fmt.Println(output)
+		} else {
+			fmt.Println(i)
+		}
+	}
 }
 
 // don't touch below this line
