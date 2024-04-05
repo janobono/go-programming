30f094171c906a2865dec2bd0b1f01c15c83a4d2
janobono <janobono@gmail.com>
2024-04-05 15:12:33 +0200

    Project create users
    Signed-off-by: janobono <janobono@gmail.com>

---
+++ project/04-create-users/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"database/sql"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
+	"github.com/janobono/go-programming/project/internal/database"
+	"github.com/joho/godotenv"
+	"log"
+	"net/http"
+	"os"
+
+	_ "github.com/lib/pq"
+)
+
+type apiConfig struct {
+	DB *database.Queries
+}
+
+func main() {
+	godotenv.Load()
+
+	portString := os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("PORT is not found in the environment")
+	}
+
+	dbURL := os.Getenv("DATABASE_URL")
+	if dbURL == "" {
+		log.Fatal("DATABASE_URL environment variable is not set")
+	}
+
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	dbQueries := database.New(db)
+
+	apiCfg := apiConfig{
+		DB: dbQueries,
+	}
+
+	router := chi.NewRouter()
+
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+
+	v1Router := chi.NewRouter()
+
+	v1Router.Post("/users", apiCfg.handlerUsersCreate)
+
+	v1Router.Get("/healthz", handlerReadiness)
+	v1Router.Get("/err", handlerErr)
+
+	router.Mount("/v1", v1Router)
+
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
+	}
+
+	log.Printf("Server starting on port %v\n", portString)
+	log.Fatal(srv.ListenAndServe())
+}
--- project/README.md
+++ project/README.md
@@ -3,3 +3,4 @@
 - [01-setup](./01-setup/README.md)
 - [02-boilerplate](./02-boilerplate/README.md)
 - [03-postgres](./03-postgres/README.md)
+- [04-create-users](./04-create-users/README.md)
